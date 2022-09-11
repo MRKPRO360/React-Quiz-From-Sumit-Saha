5d571fab629c8760307c5cb50face334ddf8e7be
Rezaul <mdrezaulkarrim@gmail.com>
2022-09-11 19:45:02 +0600

    Routes are protected (:

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Result from "./pages/Result";
 import Signup from "./pages/Signup";
 
 import { AuthProvider } from "../context/AuthContext";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 function App() {
   return (
@@ -16,10 +18,21 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+            <Route element={<PublicRoute />}>
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+
+            <Route element={<PrivateRoute />}>
+              <Route path="/quiz" element={<Quiz />} />
+            </Route>
+
+            <Route element={<PrivateRoute />}>
+              <Route path="/result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
